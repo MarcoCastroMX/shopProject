1c5474bddd3386e21d3b40e968ff77e36222db28
Marco Castro <marco.castro.vazquez@gmail.com>
2026-03-09 10:48:04 -0600

    Sistema de proteccion por roles

--- src/main/java/com/marco/shopProject/security/configuration/SecurityConfig.java
+++ src/main/java/com/marco/shopProject/security/configuration/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.marco.shopProject.security.configuration;
 
+import com.marco.shopProject.enums.RolesEnum;
+import com.marco.shopProject.rol.entity.Rol;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +33,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http){
         http    .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.DELETE,"/api/ventas/**").hasAuthority(RolesEnum.ROLE_MANAGER.name())
+                        .requestMatchers("/api/ventas/**").hasAuthority(RolesEnum.ROLE_USER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/api/sucursales/**").hasAuthority(RolesEnum.ROLE_MANAGER.name())
+                        .requestMatchers("/api/sucursales/**").hasAuthority(RolesEnum.ROLE_USER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/api/productos/**").hasAuthority(RolesEnum.ROLE_MANAGER.name())
+                        .requestMatchers("/api/productos/**").hasAuthority(RolesEnum.ROLE_USER.name())
+                        .requestMatchers("/api/estadistica/**").hasAuthority(RolesEnum.ROLE_MANAGER.name())
+                        .requestMatchers("/api/usuarios/**").hasAuthority(RolesEnum.ROLE_ADMIN.name())
+                        .requestMatchers("/api/roles/**").hasAuthority(RolesEnum.ROLE_ADMIN.name())
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
@@ -40,5 +54,13 @@ public class SecurityConfig {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy(){
+        return RoleHierarchyImpl.withRolePrefix("")
+                .role(RolesEnum.ROLE_ADMIN.name()).implies(RolesEnum.ROLE_MANAGER.name())
+                .role(RolesEnum.ROLE_MANAGER.name()).implies(RolesEnum.ROLE_USER.name())
+                .build();
     }
 }
